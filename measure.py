66541f7f813505ea499b51f22b1e953f9891d550
mtsz-thiago <mtsz.thiago@gmail.com>
2023-11-29 16:10:06 +0000

    adicionando dimensões dos grafos

--- measure.py
+++ measure.py
@@ -8,6 +8,11 @@ class PerformanceMeasure:
     elapsed_time: float
     cpu_time: float
     memory_usage: float
+    input_V: int = None
+    input_E: int = None
+    output_V: int = None
+    output_E: int = None
+    root_node: int = None
 
 def measure_performance(func, verbose=False):
     def wrapper(*args, **kwargs):
@@ -24,6 +29,13 @@ def measure_performance(func, verbose=False):
         memory_usage = (end_resources.ru_maxrss - start_resources.ru_maxrss)
         measure = PerformanceMeasure(elapsed_time, cpu_time, memory_usage)
 
+        g = args[0]
+        measure.input_E = g.get_num_edges()
+        measure.input_V = g.get_num_vertices()
+        measure.output_E = result.get_num_edges()
+        measure.output_V = result.get_num_vertices()
+        measure.root_node = args[1]
+        
         if verbose:
             print(f"Elapsed Time: {elapsed_time} seconds")
             print(f"CPU Time: {cpu_time} seconds")
